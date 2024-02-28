4c88abd94ea4349c46d5dd1ec6bc860813715c1c
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-02-28 17:30:45 -0500

    Agregar primer archivo .tsx para probar funcionalidad del componente de la carpeta pages

---
+++ src/pages/Listado.tsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Listado = () => {
+    return(
+        <>
+            <h1>Pokemon Diego</h1>
+        </>
+    );
+}
+
+export default Listado;
