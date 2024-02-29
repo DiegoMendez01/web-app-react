a595e55001ba8ed7410cadf23670d58ed8b24f26
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-02-29 17:00:29 -0500

    Pruebas de las card con data fija para una visualización pre-consumo

--- src/pages/Listado.tsx
+++ src/pages/Listado.tsx
@@ -1,6 +1,7 @@
 import react from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Listado = () => {
@@ -11,38 +12,50 @@ const Listado = () => {
                 <div className="content">
                     <div className="row gap-3">
                         <Card className="mx-auto" style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Header>Tipo: Fantasma</Card.Header>
+                        <Card.Img variant="top" src="https://img.pokemondb.net/sprites/black-white/anim/normal/gengar.gif" />
                         <Card.Body>
                             <Card.Title className="text-center">Gengar</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
+                            <ListGroup>
+                                <ListGroup.Item>HP: ##</ListGroup.Item>
+                                <ListGroup.Item>Ataque: ##</ListGroup.Item>
+                                <ListGroup.Item>Defensa: ##</ListGroup.Item>
+                                <ListGroup.Item>E. Ataque: ##</ListGroup.Item>
+                                <ListGroup.Item>E. Defensa: ##</ListGroup.Item>
+                                <ListGroup.Item>Velocidad: ##</ListGroup.Item>
+                            </ListGroup>
                         </Card.Body>
                         </Card>
 
                         <Card className="mx-auto" style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Header>Tipo: Fantasma</Card.Header>
+                        <Card.Img variant="top" src="https://img.pokemondb.net/sprites/black-white/anim/normal/haunter.gif" />
                         <Card.Body>
-                            <Card.Title className="text-center">Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
+                            <Card.Title className="text-center">Haunter</Card.Title>
+                            <ListGroup>
+                                <ListGroup.Item>HP: ##</ListGroup.Item>
+                                <ListGroup.Item>Ataque: ##</ListGroup.Item>
+                                <ListGroup.Item>Defensa: ##</ListGroup.Item>
+                                <ListGroup.Item>E. Ataque: ##</ListGroup.Item>
+                                <ListGroup.Item>E. Defensa: ##</ListGroup.Item>
+                                <ListGroup.Item>Velocidad: ##</ListGroup.Item>
+                            </ListGroup>
                         </Card.Body>
                         </Card>
 
                         <Card className="mx-auto" style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Header>Tipo: Fantasma</Card.Header>
+                        <Card.Img variant="top" src="https://img.pokemondb.net/sprites/black-white/anim/normal/gastly.gif" />
                         <Card.Body>
-                            <Card.Title className="text-center">Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
+                            <Card.Title className="text-center">Gastly</Card.Title>
+                            <ListGroup>
+                                <ListGroup.Item>HP: ##</ListGroup.Item>
+                                <ListGroup.Item>Ataque: ##</ListGroup.Item>
+                                <ListGroup.Item>Defensa: ##</ListGroup.Item>
+                                <ListGroup.Item>E. Ataque: ##</ListGroup.Item>
+                                <ListGroup.Item>E. Defensa: ##</ListGroup.Item>
+                                <ListGroup.Item>Velocidad: ##</ListGroup.Item>
+                            </ListGroup>
                         </Card.Body>
                         </Card>
                     </div>
