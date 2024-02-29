f99cec11c83ff6913869134e197cf6a152e1e38b
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-02-29 16:48:47 -0500

    Agregar en la vista de listado las cards para los pokemonos

--- src/pages/Listado.tsx
+++ src/pages/Listado.tsx
@@ -1,10 +1,53 @@
 import react from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Listado = () => {
     return(
         <>
             <h1>Pokemon Diego</h1>
+            <div className="content-wrapper">
+                <div className="content">
+                    <div className="row gap-3">
+                        <Card className="mx-auto" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title className="text-center">Gengar</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                        </Card>
+
+                        <Card className="mx-auto" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title className="text-center">Card Title</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                        </Card>
+
+                        <Card className="mx-auto" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title className="text-center">Card Title</Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
